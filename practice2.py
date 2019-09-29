fa5f05a400382146432fa27369ad677871b7f209
eprj453 <eprj453@gmail.com>
2019-09-30 05:56:17 +0900

    0930_01 commit:

---
+++ practice2.py
@@ -0,0 +1,39 @@
+# print((5+1)%6 + (4+1)%6)
+
+
+# n = 4 # 배열 크기
+# m = 9
+# print('몫 : ',n//m)
+# print('나머지 : ',n%m)
+# 현재 인덱스 1
+# 총 이동 횟수 8
+# 제한 칸 6
+
+# (r - (s - x)) // r
+# for i in range(1, 30): # 감소로 시작
+#     n = (10 + (i-6)) // 10 # 몫
+#     m = (10 + (i-6)) % 10
+#     if n % 2:
+#         print(n)
+#         print(m)
+#     else:
+#         print(n)
+#         print(10 - m)
+#     print()
+# print('change')
+#
+# for i in range(1, 20): # 증가로 시작
+#     n = ((i+2) // 7)
+#     m = ((i+2) % 7)
+#     if n % 2:
+#         print(n)
+#         print(7-m)
+#     else:
+#         print(n)
+#         print(m)
+#     print()
+
+# print(9//5)
+# print(9%5)
+print(0 // 3)
+print(0 % 3)
