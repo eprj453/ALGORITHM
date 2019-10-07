207297e23dbb543088f3b94df4f69a23fc54b811
eprj453 <eprj453@gmail.com>
2019-10-07 21:59:47 +0900

    1007_01 commit

--- BAEKJOON/14502_연구소.py
+++ BAEKJOON/14502_연구소.py
@@ -1,6 +1,7 @@
 import time
 
 def spread_virus(temp):
+    # print('active')
     global min_spread
 
     v_visited = []
@@ -8,43 +9,48 @@ def spread_virus(temp):
         v_temp = [False] * m
         v_visited.append(v_temp)
 
-    for i in range(3):
-        maps[temp[i][0]][temp[i][1]] = 1
-    # print(maps)
+    # for i in range(3):
+    #     maps[temp[i][0]][temp[i][1]] = 1
+
     v_count = 0
+    result = True
     for vi in range(len(virus)):
-        # if v_count >= min_spread:
-        #     return
         v_x, v_y = virus[vi][0], virus[vi][1]
         v_visited[v_x][v_y] = True
         q = [[v_x, v_y]]
+        if v_count == -1:
+            break
         while q:
+            if v_count >= min_spread:
+                v_count = -1
+                break
             x, y = q[0][0], q[0][1]
             for k in range(len(dx)):
                 if 0 <= x + dx[k] < n and 0 <= y + dy[k] < m:
-                    if maps[x+dx[k]][y+dy[k]] != 1 and v_visited[x+dx[k]][y+dy[k]] == False:
+                    if maps[x+dx[k]][y+dy[k]] == 0 and v_visited[x+dx[k]][y+dy[k]] == False and [x+dx[k], y+dy[k]] not in temp:
                         v_visited[x+dx[k]][y+dy[k]] = True
                         q.append([x+dx[k], y+dy[k]])
                         v_count += 1
             q.pop(0)
-    # print(v_count)
-    min_spread = min(min_spread, v_count)
 
-    for i in range(3):
-        maps[temp[i][0]][temp[i][1]] = 0
 
-    return
+    if v_count == -1:
+        return
+    else:
+        min_spread = min(min_spread, v_count)
+        return
+
 
 
 
 def comb(k, start):
+    global min_ans
     if k == r:
-        # print(temp)
         spread_virus(temp)
         return
 
-    for i in range(start, len(walls)):
-        temp.append(walls[i])
+    for i in range(start, len(safe_zone)):
+        temp.append(safe_zone[i])
         comb(k+1, i+1)
         temp.pop()
 
@@ -53,31 +59,27 @@ start = time.time()
 n, m = map(int, input().split())
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 maps = [list(map(int, input().split())) for _ in range(n)]
-walls = []
 virus = []
-safes = 0
-
+safe_zone = []
 for i in range(n):
     for j in range(m):
         if maps[i][j] == 0:
-            walls.append([i, j])
-            safes += 1
-            # walls.append([i, j])
-        if maps[i][j] == 2:
+            safe_zone.append([i, j])
+        elif maps[i][j] == 2:
             virus.append([i, j])
-
-# print(virus)
-virus_spread = len(virus)
 min_spread = 64
 temp = []
 r = 3
 comb(0, 0)
-print(safes)
-print('virus_spread : ', virus_spread)
-print('min_spread : ', min_spread)
-
-# print(min_spread+virus_spread)
-print(safes - (min_spread + 3))
+# print(safes)
+# print('virus_spread : ', virus_spread)
+# print('min_spread : ', min_spread)
+# print('safe : ', safe)
+# # print(min_spread+virus_spread)
+# print('wall : ', wall+3)
+# print('min_ans : ',min_ans)
+print(len(safe_zone) - min_spread - 3)
+# print(safes - (min_spread))
 # print(len(walls))
 # for i in range(1 << len(walls)):
 #     wall_temp = []
