b6fd982d4968f4af6ae8080d63e9324f5d2442fb
sangwon <eprj453@gmail.com>
2019-10-10 18:01:21 +0900

    1010_01 commit

---
+++ 29_day_191010/연습문제3_트리순회.py
@@ -0,0 +1,46 @@
+def pre_order(v):
+    print(v, end = ' ')
+    for i in range(2):
+        if children[v][i] != 0:
+            pre_order(children[v][i])
+
+def in_order(v):
+    if children[v][0] == 0 and children[v][1] == 0:
+        print(v, end = ' ')
+        return
+
+    for i in range(len(children[v])):
+        if i == 0 and children[v][i] != 0:
+            in_order(children[v][0])
+            print(v, end =' ')
+        if i == 1 and children[v][i] != 0:
+            in_order(children[v][1])
+
+def post_order(v):
+
+    if v != 0:
+        post_order(children[v][0])
+        post_order(children[v][1])
+        print(v, end = ' ')
+        return
+
+n = 13
+nodes = [1, 2, 1, 3, 2, 4, 3, 5, 3, 6, 4, 7, 5, 8, 5, 9, 6, 10, 6, 11, 7, 12, 11, 13]
+children = []
+for _ in range(14):
+    temp = [0,0]
+    children.append(temp)
+for i in range(0, len(nodes), 2):
+    if children[nodes[i]][0] == 0:
+        children[nodes[i]][0] = nodes[i+1]
+    else:
+        children[nodes[i]][1] = nodes[i+1]
+print('전위 순회 결과 : ',end = ' ')
+pre_order(1)
+print()
+print('중위 순회 결과 : ',end = ' ')
+in_order(1)
+print()
+print('후위 순회 결과 : ',end = ' ')
+post_order(1)
+print()
--- BAEKJOON/5427_불(미).py
+++ BAEKJOON/5427_불(미).py
@@ -1,58 +1,118 @@
-for i in range(1, int(input())+1):
+# for i in range(1, int(input())+1):
+# #     w, h = map(int, input().split())
+# #     start_x, start_y = 0, 0
+# #     maps = []
+# #     fires = []
+# #     visited = []
+# #     q = []
+# #     dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+# #     c = 0
+# #     while c < h:
+# #         temp = list(input())
+# #         temp2 = [False] * w
+# #         maps.append(temp)
+# #         visited.append(temp2)
+# #         for j in range(len(temp)):
+# #             if temp[j] == '@':
+# #                 start_x, start_y = c, j
+# #                 visited[start_x][start_y] = True
+# #             if temp[j] == '*':
+# #                 q.append([c, j, 1, 0])
+# #                 visited[c][j] = True
+# #         c += 1
+# #     q.append([start_x, start_y, 0, 0])
+# #     min_distance = 0
+# #     # 0 > 나, 1 > 불
+# #     while q:
+# #         x, y, which, count = q[0][0], q[0][1], q[0][2], q[0][3]
+# #         if (x == 0 or y == 0 or x == h-1 or y == w-1) and which == 0:
+# #             min_distance = count
+# #             break
+# #
+# #         if which == 1:
+# #             for k in range(len(dx)):
+# #                 if 0 <= x+dx[k] < h and 0 <= y+dy[k] < w:
+# #                     if maps[x+dx[k]][y+dy[k]] == '.' and visited[x+dx[k]][y+dy[k]] == False:
+# #                         q.append([x+dx[k], y+dy[k], which, count+1])
+# #                         visited[x+dx[k]][y+dy[k]] = True
+# #
+# #             q.pop(0)
+# #
+# #
+# #         elif which == 0:
+# #             for k in range(len(dx)):
+# #                 if 0 <= x+dx[k] < h and 0 <= y+dy[k] < w:
+# #                     if (x == 0 or y == 0 or x == h or y == w) and which == 0:
+# #                         min_distance = count
+# #                         break
+# #                     if maps[x+dx[k]][y+dy[k]] != '#' and visited[x+dx[k]][y+dy[k]] == False:
+# #                         q.append([x+dx[k], y+dy[k], which, count+1])
+# #                         visited[x+dx[k]][y+dy[k]] = True
+# #             q.pop(0)
+# #
+# #         if min_distance:
+# #             break
+# #     if min_distance == 0:
+# #         print('IMPOSSIBLE')
+# #     else:
+# #         print(min_distance+1)
+
+def fire_spread(my_q, fire_q):
+    global distance
+    if len(my_q) == 0:
+        return
+    if len(fire_q) == 0:
+        return
+
+    my_new_q = []
+    fire_new_q = []
+
+    for i in range(len(fire_q)):
+        x, y = fire_q[i][0], fire_q[i][1]
+        for k in range(len(dx)):
+            if 0 <= x+dx[k] < h and 0 <= y+dy[k] < w:
+                if maps[x+dx[k]][y+dy[k]] != '#' and maps[x+dx[k]][y+dy[k]] != '*':
+                    visited[x+dx[k]][y+dy[k]] = True
+                    maps[x+dx[k]][y+dy[k]] = '*'
+                    fire_new_q.append([x+dx[k], y+dy[k]])
+    print('fire_new_q : ', fire_new_q)
+    for i in range(len(my_q)):
+        x, y, z  = my_q[i][0], my_q[i][1], my_q[i][2]
+        if x == 0 or y == 0 or x == h-1 or y == w-1:
+            distance = min(distance, z+1)
+            return
+        for k in range(len(dx)):
+            if 0 <= x+dx[k] < h and 0 <= y+dy[k] < w:
+                if maps[x+dx[k]][y+dy[k]] == '.' and visited[x+dx[k]][y+dy[k]] == False:
+                    visited[x+dx[k]][y+dy[k]] = True
+                    maps[x+dx[k]][y+dy[k]] = '@'
+                    my_new_q.append([x+dx[k], y+dy[k], z+1])
+    print('my_new_q : ', my_new_q)
+    fire_spread(my_new_q, fire_new_q)
+
+
+
+dx, dy = [-1, 1, 0 ,0], [0, 0, -1, 1]
+for l in range(int(input())):
     w, h = map(int, input().split())
-    start_x, start_y = 0, 0
-    maps = []
-    fires = []
-    visited = []
-    q = []
-    dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
-    c = 0
-    while c < h:
+    me, fire, maps, visited = [], [], [], []
+    for j in range(h):
         temp = list(input())
-        temp2 = [False] * w
+        v_temp = [False] * w
+        visited.append(v_temp)
+        for k in range(w):
+            if temp[k] == '@':
+                me = [[j, k, 0]]
+                visited[j][k] = True
+            elif temp[k] == '*':
+                fire.append([j, k])
+                visited[j][k] = True
         maps.append(temp)
-        visited.append(temp2)
-        for j in range(len(temp)):
-            if temp[j] == '@':
-                start_x, start_y = c, j
-                visited[start_x][start_y] = True
-            if temp[j] == '*':
-                q.append([c, j, 1, 0])
-                visited[c][j] = True
-        c += 1
-    q.append([start_x, start_y, 0, 0])
-    min_distance = 0
-    # 0 > 나, 1 > 불
-    while q:
-        x, y, which, count = q[0][0], q[0][1], q[0][2], q[0][3]
-        if (x == 0 or y == 0 or x == h-1 or y == w-1) and which == 0:
-            min_distance = count
-            break
-
-        if which == 1:
-            for k in range(len(dx)):
-                if 0 <= x+dx[k] < h and 0 <= y+dy[k] < w:
-                    if maps[x+dx[k]][y+dy[k]] == '.' and visited[x+dx[k]][y+dy[k]] == False:
-                        q.append([x+dx[k], y+dy[k], which, count+1])
-                        visited[x+dx[k]][y+dy[k]] = True
-
-            q.pop(0)
-
-
-        elif which == 0:
-            for k in range(len(dx)):
-                if 0 <= x+dx[k] < h and 0 <= y+dy[k] < w:
-                    if (x == 0 or y == 0 or x == h or y == w) and which == 0:
-                        min_distance = count
-                        break
-                    if maps[x+dx[k]][y+dy[k]] != '#' and visited[x+dx[k]][y+dy[k]] == False:
-                        q.append([x+dx[k], y+dy[k], which, count+1])
-                        visited[x+dx[k]][y+dy[k]] = True
-            q.pop(0)
-
-        if min_distance:
-            break
-    if min_distance == 0:
+    distance = 1000000
+    fire_spread(me, fire)
+    if distance == 1000000:
         print('IMPOSSIBLE')
     else:
-        print(min_distance+1)
+        print(distance)
+
+
