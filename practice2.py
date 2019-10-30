5006e65f0aad0c38fde6eaaf8c1f995b4e6c6a9c
sangwon <eprj453@gmail.com>
2019-10-30 18:01:36 +0900

    1030_01 commit

--- practice2.py
+++ practice2.py
@@ -1,110 +1,93 @@
-import time
-
-# print((5+1)%6 + (4+1)%6)
+musicinfos = ['12:00','12:14','HELLO','C#DEFGAB', '13:00','13:05','WORLD','ABCDEF']
 
 
-# n = 4 # 배열 크기
-# m = 9
-# print('몫 : ',n//m)
-# print('나머지 : ',n%m)
-# 현재 인덱스 1
-# 총 이동 횟수 8
-# 제한 칸 6
-
-# (r - (s - x)) // r
-# for i in range(1, 30): # 감소로 시작
-#     n = (10 + (i-6)) // 10 # 몫
-#     m = (10 + (i-6)) % 10
-#     if n % 2:
-#         print(n)
-#         print(m)
+m = 'ABC'
+# 12:55 13:18
+# info_list = []
+# for i in range(0, len(musicinfos), 4):
+#     start, end = musicinfos[i], musicinfos[i+1]
+#     # print(start[3:], end[3:])
+#     if int(start[0:2])  == int(end[0:2]):
+#         play_time = int(end[3:]) - int(start[3:])
 #     else:
-#         print(n)
-#         print(10 - m)
-#     print()
-# print('change')
+#         play_time = (60 - int(start[3:])) + end[3:]
 #
-# for i in range(1, 20): # 증가로 시작
-#     n = ((i+2) // 7)
-#     m = ((i+2) % 7)
-#     if n % 2:
-#         print(n)
-#         print(7-m)
+#     codes = musicinfos[i+3]
+#     t_codes = ''
+#     if len(codes) >= play_time:
+#         t_codes = codes[0:play_time]
 #     else:
-#         print(n)
-#         print(m)
-#     print()
+#         while True:
+#             if len(t_codes) == play_time:
+#                 break
+#             for j in range(len(codes)):
+#                 if len(t_codes) == play_time:
+#                     break
+#                 t_codes += codes[j]
+#
+#
+#     info_list.append([musicinfos[i+2], t_codes])
+#
+# ans = ''
+# ans_music_len = 0
+# for i in range(0, len(info_list)):
+#     if m in info_list[i][1]:
+#         if len(info_list[i][1]) > ans_music_len:
+#             ans_music_len = len(info_list[i][1])
+#             ans = info_list[i][0]
+#
+# if not ans:
+#     print(None)
+# else:
+#     print(ans)
+            # ans =
+# print(','.join(musicinfos))
 
-# print(9//5)
-# print(9%5)
-#
-# list1 = [0,1,2,3,4,5]
-# ans = {0,1,2,3,4,5}
-# n, c = 0, 5
-#
-# for j in list(ans):
-#     for lis in list1:
-#         if int((str(lis) + str(j))) == 1111 or int((str(j) + str(lis))) == 1111:
-#             print('c')
-#         ans.add(int((str(lis) + str(j))))
-#         ans.add(int((str(j) + str(lis))))
-#     # ans.add(list)
-#     # c += 1
-# print(len(ans))
-# print(ans)
-#
-# start = time.time()
-# set_num = 793881
-# n = 1
-# c = set_num // 2
-# t_list = []
-# while n <= c:
-#     if set_num % n == 0:
-#         t_list.append(n)
-#     n += 1
-#
-# print(t_list)
-# print('time : {}'.format(time.time() - start))
-#
-# list1 = [5,4,3,2,1]
-# list1 = list1[2:]
-# print(list1)
-#
-# def comb(s, start):
-#     global cnt
-#     cnt += 1
-#     if s == r:
-#         print(temp)
-#         return
-#
-#     else:
-#         for i in range(start, n):
-#             temp.append(list1[i])
-#             comb(s+1, i+1)
-#             temp.pop()
-#             # if visited[i]: continue
-#             # else:
-#             #     visited[i] = True
-#             #     temp[s] = list1[i]
-#             #     comb(s+1)
-#             #     visited[i] = False
-#
-# list1 = list(range(64))
-# n = len(list1)
-# temp = []
-# r = 3
-# visited = [False] * n
-# cnt = 0
-# comb(0, 0)
-# print(cnt)
-#
-# a = '(3+4)*2'
-# print(int(a))
+info_list = []
+play_time = 0
+for i in range(0, len(musicinfos), 4):
+    start, end = musicinfos[i], musicinfos[i+1]
+    if int(start[0:2])  == int(end[0:2]):
+        play_time = int(end[3:]) - int(start[3:])
+    else:
+        play_time = (60 - int(start[3:])) + int(end[3:])
+
+    codes = musicinfos[i+3]
+    t_codes = ''
+    if len(codes) >= play_time:
+        t_codes = codes[0:play_time]
+    else:
+        while True:
+            if len(t_codes) == play_time:
+                break
+            for j in range(len(codes)):
+                if len(t_codes) == play_time:
+                    break
+                t_codes += codes[j]
 
 
-# t = (1,2,3)
-# print(t[2])
+    info_list.append([musicinfos[i+2], t_codes])
+print(info_list)
+ans = ''
+ans_music_len = 0
+for i in range(0, len(info_list)):
+    if m in info_list[i][1] and :
+        if len(info_list[i][1]) > ans_music_len:
+            ans_music_len = len(info_list[i][1])
+            ans = info_list[i][0]
 
-#
-# for i in list1:
-#     print(a+i)
+if not ans:
+    answer = None
+else:
+    answer = ans
+
+print(answer)
+
+'''
+
+['12:00','12:14','HELLO','CDEFGAB', '13:00','13:05','WORLD','ABCDEF']
+['03:00','03:30','FOO','CC#B', '04:00','04:08','BAR','CC#BCC#BCC#B']
+['12:00','12:14','HELLO','C#DEFGAB', '13:00','13:05','WORLD','ABCDEF']
+
+
+'''
