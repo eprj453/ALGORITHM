d307bc249ea39b01474ddf9fbd40289b2fc5499c
YaeHoChoi <swexpert93@naver.com>
2019-12-10 09:14:49 +0900

    commit

--- 계절학기/day1/3816.아나그램.py
+++ 계절학기/day1/3816.아나그램.py
@@ -13,7 +13,7 @@ for i in range(1, int(input())+1):
     alpha_count = [0] * 26
     for s in s1:
         alpha_count[alphas.index(s)] += 1
-    # print(alpha_count)
+
     temp_count = [0] * 26
     j = 0
     while j < len(s1):
@@ -22,7 +22,6 @@ for i in range(1, int(input())+1):
 
     k = 0
     while k < len(s2) - len(s1) + 1:
-        # print(temp_count)
         if alpha_count == temp_count:
             ans += 1
 
@@ -31,13 +30,8 @@ for i in range(1, int(input())+1):
 
         temp_count[alphas.index(s2[k])] -= 1
         temp_count[alphas.index(s2[k+len(s1)])] += 1
-        # print(s2[k])
-        # print(alphas.index(s2[k]))
-        # print(s2[alphas.index(s2[k+len(s1)])])
-        # temp_count[alphas.index(s2[k])] -= 1
-        # temp_count[alphas.index(s2[k + len(s1)])] += 1
+
         k += 1
-        # print('------------------------')
     print('#{} {}'.format(i, ans))
 
 
