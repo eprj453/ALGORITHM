b370aaa82e1999a99ca4db4c7952d4e6a96b6a8d
eprj453 <eprj453@gmail.com>
2020-01-24 23:42:57 +0900

    200124_Commit

--- C++/Algorithm/Algorithm/BOJ_17141_게리맨더링.cpp
+++ C++/Algorithm/Algorithm/BOJ_17141_게리맨더링.cpp
@@ -1,103 +1,81 @@
 #include <iostream>
-#include <queue>
+#include <stack>
 #include <vector>
 using namespace std;
 int n;
 int ans = 1000;
-bool connect[11][11] = {false,};
-int population[11] = {};
+bool connect[10][10] = {false,};
+bool visited[10];
+int population[10] = {};
 vector <int> set;
+int dfsSize;
 
 int min(int x, int y) {
 	return x > y ? y : x;
 }
 
-bool findConnect(bool Acheck[11], bool Bcheck[11], queue <int> Asubset, queue <int> Bsubset) {
-
-	int Asize = Asubset.size() -1, Bsize = Bsubset.size()-1;
-	//int Atemp = 1, Btemp = 1;
-	bool Avisited[10] = {false,};
-	bool Bvisited[10] = {false,};
-	Avisited[Asubset.front()] = true;
-	Bvisited[Bsubset.front()] = true;
-	bool Aresult = true;
-	bool Bresult = true;
-	while (Asubset.size() > 0) {
-		int x = Asubset.front();
-		bool t = false;
-		for (int k = 0; k < n; k++) {
-			if (x != k && connect[x][k] == true  && Acheck[k] == true && Bcheck[k] == false && Avisited[k] == false) {
-				//Asubset.push(k);
-				Asize--;
-				Avisited[k] = true;
-
-			};
-		}
-		Asubset.pop();
-	}
-
-	if (Asize == 0) {
-		while (Bsubset.size() > 0) {
-			int x = Bsubset.front();
-			for (int k = 0; k < n; k++) {
-				if (x != k && connect[x][k] == true && Bcheck[k] == true && Acheck[k] == false && Bvisited[k] == false) {
-				//	Bsubset.push(k);
-					Bvisited[k]= true;
-					Bsize--;
-				} else Bresult = false;
+bool DFS(bool check[10], stack <int> subset, bool visited[10]) {
+	int subsetSize = subset.size();
+	while (subset.size() > 0) {
+		int x = subset.top();
+		visited[x] = true;
+		for (int k = 0; k < n ; k++) {
+			if (x != k && connect[x][k] == true && check[k] == true && visited[k] == false) {
+				visited[k] = true;
+				subset.push(k);
+				dfsSize++;
+				DFS(check, subset, visited);
 			}
-			Bsubset.pop();
 		}
+
+		subset.pop();
 	}
 
-	if (Asize == 0 && Bsize == 0) {
-		cout << "true" << endl;
-		return true;
-	} else return false;
+	if (dfsSize == subsetSize) return true;
+	else return false;
+	
 }
 
 void findSubset() {
 	for (int i = 0; i < (1 << n); i++) {
-		queue <int> Aset;
+		stack <int> Aset;
+		int Ap = 0; 
 		bool Avisit[10] = {false,};
 		for (int j = 0; j < n; j++) {
 			if (i & (1 << j)) {
 				Aset.push(set[j]);
-				Avisit[j] = true;
+				Avisit[set[j]] = true;
+				Ap += population[set[j]];
 			}
 		}
 		if (0 < Aset.size() && Aset.size() < n) {
-			queue <int> Bset;
-			bool Bvisit[10] = {false,};
-			for (int k = 0; k < n; k++) {
-				if (Avisit[k] == false) {
-					Bset.push(set[k]);
-					Bvisit[k] = true;
+			bool visited[10] = {false,};
+			dfsSize = 1;
+			bool Aresult = DFS(Avisit, Aset, visited);
+			if (Aresult) {
+				int Bp = 0;
+				bool visited[10] = {false,};
+				stack <int> Bset;
+				bool Bvisit[10] = {false,};
+				for (int k = 0; k < n; k++) {
+					if (Avisit[k] == false) {
+						Bset.push(set[k]);
+						Bvisit[set[k]] = true;
+						Bp += population[set[k]];
+					}
+				}
+				dfsSize = 1;
+				bool Bresult = DFS(Bvisit, Bset, visited);
+
+				if (Aresult && Bresult) {
+					ans = min(ans, abs(Ap - Bp));
 				}
 			}
-			
-			if (findConnect(Avisit, Bvisit, Aset, Bset)) {
-				int Ap = 0, Bp = 0;
-				while (Aset.size() > 0) {
-					Ap += population[Aset.front()];
-					cout << Aset.front() << " ";
-					Aset.pop();
-				} cout << endl;
-
-				while (Bset.size() > 0) {
-					Bp += population[Bset.front()];
-					cout << Bset.front() << " ";
-					Bset.pop();
-				} cout << endl;
-
-				ans = min(ans, abs(Ap-Bp));
-			}		
 		}
 	}
 }
 
 int main(){
-	//int n;
 	int temp;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
@@ -116,21 +94,9 @@ int main(){
 		}
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << connect[i][j] << " ";
-		} cout << endl;
-	}
 	findSubset();
-	cout << ans << endl;
+	if (ans == 1000) cout << -1 << endl;
+	else cout << ans << endl;
 	return 0;
 }
 
-6
-2 2 2 2 2 2
-1 3
-1 4
-1 1
-1 2
-1 6
-1 5
