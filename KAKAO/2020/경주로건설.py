576581ebdebac058fc97b6c0d1e7408773f31fc6
eprj453 <eprj453@gmail.com>
2020-10-06 00:18:16 +0900

    201005_commit2

--- KAKAO/2020/경주로건설.py
+++ KAKAO/2020/경주로건설.py
@@ -1,47 +1,33 @@
+def dijkstra(board, dijk, q, dir_dict):
+    n = len(board)
+    while q:
+        x, y, value, d = q.pop(0)
+        for dir in dir_dict.keys():
+            dx, dy = x + dir[0], y + dir[1]
+            if 0 <= dx < n and 0 <= dy < n and not board[dx][dy]:
+                next_dir = dir_dict[dir]
+                cost = 100 if d == next_dir else 600
+
+                if dijk[dx][dy] >= value + cost or dijk[dx][dy] == -1:
+                    dijk[dx][dy] = value + cost
+                    q.append([dx, dy, value + cost, next_dir])
+
+    return dijk[n - 1][n - 1]
+
+
 def solution(board):
     answer = 0
     n = len(board)
-    dijk = [[n**2 * 500] * n for _ in range(n)]
+    dijk = [[-1] * n for _ in range(n)]
     dijk[0][0] = 0
     dir_dict = {
-        (0 ,1): 1,
+        (0, 1): 1,
         (1, 0): 2,
         (0, -1): 3,
         (-1, 0): 4
     }
 
-    q = [[0, 0, 0]]
+    result1 = dijkstra(board, dijk, [[0, 0, 0, 1]], dir_dict)
+    result2 = dijkstra(board, dijk, [[0, 0, 0, 2]], dir_dict)
 
-    while q:
-        x, y, d = q.pop(0)
-        for dir in dir_dict.keys():
-            dx, dy = x + dir[0], y + dir[1]
-            if 0 <= dx < n and 0 <= dy < n and board[dx][dy] == 0:
-                if d == 0 or d == dir_dict[dir]:
-                    cost = 100
-                    next_dir = dir_dict[dir] if d == 0 else d
-                else:
-                    cost = 600
-                    next_dir = dir_dict[dir]
-                if dijk[dx][dy] >= dijk[x][y] + cost:
-                    dijk[dx][dy] = dijk[x][y] + cost
-                    q.append([dx, dy, dir_dict[dir]])
-
-                # else:
-                #     if dijk[dx][dy] >= dijk[x][y] + 600:
-                #         dijk[dx][dy] = dijk[x][y] + 600
-                #         q.append([dx, dy, dir_dict[dir]])
-
-    for dij in dijk:
-        print(dij)
-    answer = dijk[n-1][n-1]
-
-    return answer
-
-# print(solution([[0,0,0],[0,0,0],[0,0,0]]))
-# print(solution([[0,0,0,0],[0,0,0,0],[0,0,0,0], [0,0,0,0]]))
-
-arr = solution(([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]))
-print(arr)
-# print(solution([[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]]))
-# print(solution([[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]]))
+    return min(result1, result2)
